596c320d464618bd4fca74dc6852b4e968b955f8
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2019-05-09 00:03:50 -0300

    Created defines for card suits chars

---
+++ include/suits.h
@@ -0,0 +1,35 @@
+#ifndef SUITS_H
+#define SUITS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * @brief Caracter unicode U+2660
+ * Representa o naipe de espadas
+ */
+#define SUIT_SPD "\u2660"
+
+/**
+ * @brief Caracter unicode U+2666
+ * Representa o naipe de ouro
+ */
+#define SUIT_DMD "\u2666"
+
+/**
+ * @brief Caracter unicode U+2663
+ * Representa o naipe de espadas
+ */
+#define SUIT_CLB "\u2663"
+
+/**
+ * @brief Caracter unicode U+26605
+ * Representa o naipe de copas
+ */
+#define SUIT_HRT "\u2665"
+
+#if __cplusplus
+}
+#endif // _cplusplus
+#endif // SUITS_H
