de4f687a4c1389e7c2653470b758f53d0ecdc874
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2019-05-13 17:11:07 -0300

    Prints splash

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "splash.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+    printf("%s", splash_screen);
+    printf("\n");
     return EXIT_SUCCESS;
 }
